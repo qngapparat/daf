a0970326523ae65a8e0ffed8db67bc3b5cbee87e
qngapparat <jakob.wallnoefer@outlook.com>
2020-04-29 18:50:38 +0200

    fix index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const amazon = require('./amazon')
 /**
  * 
  * @param {string} fpath Path to .js file
@@ -5,6 +6,11 @@
  * @param {string} outpath Where faasified files should be written
  */
 function main(fpath, linenum, outpath) {
+
+  if(fpath == null) throw new Error("Specify fpath")
+  if(linenum == null) throw new Error("Specify linenum")
+  if(outpath == null) throw new Error("Specify outpath")
+
   return amazon({
     '--outpath': outpath,
     '--linenum': linenum,
